fa6c35d8fa2bfc899267a36e44b6a4717d89d704
vitaly <vitaly.zutov@yandex.ru>
2022-12-20 21:33:44 +0700

    Задача 11. Выводим случайное 3-х значное число, удаляем из него 2-ю цифру

---
+++ seminar_2/task_3/Program.cs
@@ -0,0 +1,21 @@
+﻿/* **Задача 11:**
+Напишите программу, которая выводит случайное трёхзначное число и удаляет 
+вторую цифру этого числа.
+456 -> 46
+782 -> 72
+918 -> 98
+*/
+
+using static System.Console;
+Clear();
+int number = new Random().Next(100, 1000);
+WriteLine(number);
+
+int sot = number / 100;
+//целое из = (number - sot*100) / 10;
+int edin = number % 10;
+//Write(sot);
+//WriteLine(edin);
+
+//Если не строками, а собрать число
+WriteLine(sot*10 + edin);
