02756f3a492cdd65c9631c2ffc2e0fe32282ca10
vitaly <vitaly.zutov@yandex.ru>
2022-12-24 19:14:03 +0700

    Задача. Про двух стрелков, опизание задачи внутри

---
+++ seminar_2/task_7/Program.cs
@@ -0,0 +1,18 @@
+﻿//   Два стрелка
+//  На вход - сколько банок каждый стрелок сбил
+//  /*
+//  На выход - сколько всего банок было
+//  */
+//  В последнюю попали одновременно.
+
+// Задача непонятно зачем =)
+
+using static System.Console;
+
+Write("Input first number: ");
+int FirstNumber = int.Parse(ReadLine()!);
+Write("Input second number: ");
+int SecondNumber = int.Parse(ReadLine()!);
+
+int Result = FirstNumber + SecondNumber - 1;
+WriteLine(Result);
