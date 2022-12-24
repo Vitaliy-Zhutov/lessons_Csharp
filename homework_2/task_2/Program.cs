c1fea7f0bc3376433f121a4a9be2c4e7feccb6c4
vitaly <vitaly.zutov@yandex.ru>
2022-12-24 21:19:17 +0700

    Задача 13. Выводим 3-ю цифру заданного числа, или сообщаем что его нет

---
+++ homework_2/task_2/Program.cs
@@ -0,0 +1,24 @@
+﻿/* Задача 13: Напишите программу, которая выводит третью цифру заданного числа или сообщает, 
+что третьей цифры нет.
+
+645 -> 6
+78 -> третьей цифры нет
+32679 -> 6
+*/
+
+using static System.Console;
+Clear();
+
+Write("Введите число от -2147483648 до 2147483647: ");
+int Number = int.Parse(ReadLine()!);
+
+if (Number < 100)
+
+{
+    WriteLine("В веденом числе нет третьей цифры");
+    return -1;
+}
+
+int ThreeNumber = Number % 1000 / 100;
+Write("Третья цифра = {0}", ThreeNumber);
+return ThreeNumber;
