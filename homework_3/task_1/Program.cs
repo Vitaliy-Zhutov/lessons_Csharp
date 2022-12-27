10be31487c3f71b679bba94aa7de1a1e6dad39f3
vitaly <vitaly.zutov@yandex.ru>
2022-12-27 22:35:41 +0700

    Задача 19. Проверка 5-ти значного числа на палиндром

---
+++ homework_3/task_1/Program.cs
@@ -0,0 +1,31 @@
+﻿/*
+Задача 19
+Напишите программу, которая принимает на вход пятизначное число и проверяет, 
+является ли оно палиндромом.
+14212 -> нет
+12821 -> да
+23432 -> да
+*/
+
+using static System.Console;
+Clear();
+
+Write("Введите 5-ти значное число: ");
+string number = Console.ReadLine()!;
+int len = number.Length;
+
+if (len == 5)
+{
+    if (number[0] == number[4] && number[1] == number[3])
+    {
+        WriteLine($"{number} - Палиндром");
+    }
+    else
+    {
+        WriteLine($"{number} - НЕ палиндром");
+    }
+}
+else
+{
+    WriteLine($"ОШИБКА: {number} - не является пятизначным");
+}
