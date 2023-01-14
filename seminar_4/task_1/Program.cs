cb1cb15d453696c67774a658e708e0bd777932f3
vitaly <vitaly.zutov@yandex.ru>
2023-01-14 21:41:14 +0700

    Задача 1. Найти сумму всех чисел от 1 до А

---
+++ seminar_4/task_1/Program.cs
@@ -0,0 +1,25 @@
+﻿// Найти сумму всех чисел от 1 до А.
+
+
+using static System.Console;
+
+Write("Input number: ");
+int number = int.Parse(ReadLine()!);
+
+int sum = GetSum(number);
+
+WriteLine($"Sum number 1..A = {sum}");
+
+
+int GetSum(int A)
+{
+    int resultSum = 0;
+    while (A > 0)
+    {
+        resultSum += A;
+        // resultSum = resultSum + A
+        A--;
+        // A = A - 1 
+    }
+    return resultSum;
+}
