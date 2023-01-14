dbaa725675fac6d281b0609d2acabfecb4a2dde7
vitaly <vitaly.zutov@yandex.ru>
2023-01-15 00:02:35 +0700

    Задача 3. На входе число N, на выходе произведение чисел от 1 до N

---
+++ seminar_4/task_3/Program.cs
@@ -0,0 +1,28 @@
+﻿// Напишите программу которая на вход принимает число N 
+// Выдает же произведение чесел от 1 до N
+// 4 -> 24
+// 5 -> 120
+
+using static System.Console;
+
+Write("Input number: ");
+int number = int.Parse(ReadLine()!);
+
+int sum = GetSum(number);
+
+WriteLine($"Sum number 1..N = {sum}");
+
+int GetSum (int N)
+{
+    int resultSum = 1;
+
+    while (N > 0)
+    {
+        {
+        resultSum *= N;
+        N--;
+        }
+    }
+
+    return resultSum;
+}
