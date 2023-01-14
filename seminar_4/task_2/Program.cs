bf7d91b3c14bebec9825c84fa6cc97b98033175b
vitaly <vitaly.zutov@yandex.ru>
2023-01-14 23:54:48 +0700

    Задача 2. На входе число, на выходе количество цифр в числе

---
+++ seminar_4/task_2/Program.cs
@@ -0,0 +1,28 @@
+﻿// Напишите программу, которая на вход принимает число и выводит
+// Количество цифр в числе.
+// 456 -> 3
+// 78 -> 2
+// 89126 -> 5
+
+using static System.Console;
+
+Write("Input number: ");
+int number = int.Parse(ReadLine()!);
+
+int count = numCount(number);
+
+WriteLine($"Number of digits in a number = {count}");
+
+
+int numCount(int num)
+{
+    int resultCount = 0;
+
+    while (num > 0)
+    {
+        num /= 10;
+        resultCount++;
+    }
+
+    return resultCount;
+}
