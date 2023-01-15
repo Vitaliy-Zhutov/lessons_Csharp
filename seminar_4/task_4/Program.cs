cad8d34be2ffdec6260aa7231df0c24d400cf5cc
vitaly <vitaly.zutov@yandex.ru>
2023-01-15 22:12:25 +0700

    Задача 4. Программа выводит массив из 8 эл. из 0 и 1 в случайном порядке

---
+++ seminar_4/task_4/Program.cs
@@ -0,0 +1,35 @@
+﻿// Напишите программу, которая выводит массив из 8 элементов
+// заполненный нулями и единицами в случайном порядке.
+
+using static System.Console;
+Clear();
+
+
+int[] arrey = IntBinaryArrey();
+PrintArrey(arrey);
+
+
+int[] IntBinaryArrey()
+{
+    int[] res = new int[8];
+
+    for (int i = 0; i < res.Length; i++)
+    {
+        Random ran = new Random();
+        res [i] = ran.Next(0, 2);
+    }
+
+    return res;
+}
+
+void PrintArrey(int[] arr)
+{
+    int count = arr.Length;
+    
+    for (int i = 0; i < count; i++)
+    {
+        Console.Write($"{arr[i]} ");
+    }
+    
+    Console.WriteLine();
+}
