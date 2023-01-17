8164fc7925fe362396aafa656a97b4149ceb1a1b
vitaly <vitaly.zutov@yandex.ru>
2023-01-17 18:18:37 +0700

    Задача 27. Метод принимает число и выдает сумму цифр в числе

---
+++ homework_4/task_2/Program.cs
@@ -0,0 +1,47 @@
+﻿/*
+Задача 27: Напишите программу, реализующую метод, который принимает на вход число 
+и выдаёт сумму цифр в числе.
+452 -> 11
+82 -> 10
+9012 -> 12
+*/
+
+using static System.Console;
+Clear();
+
+int number = ImputNumber("Imput number: ");
+int len = NumberLen(number);
+SumNumbers(number, len);
+
+
+// Функция ввода
+int ImputNumber(string message)
+{
+    Console.Write(message);
+    return int.Parse(ReadLine()!);
+}
+
+// Функция подсчета цифр в числе
+int NumberLen(int a)
+{
+    int index = 0;
+    while (a != 0)
+    {
+        a /= 10;
+        index++;
+    }
+    return index;
+}
+
+// Функция вывода суммы цифр в числе
+void SumNumbers(int n, int len)
+{
+    int sum = 0;
+    for (int i = 1; i <= len; i++)
+    {
+        sum += n % 10;
+        n /= 10;
+    }
+    Console.WriteLine($"Sum of digits = {sum}");
+}
+
