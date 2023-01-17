fdf4f7fc6b4aebfb15b6d3e434e0f8b5eb2cdbb9
vitaly <vitaly.zutov@yandex.ru>
2023-01-17 19:14:38 +0700

    Задача 29. Метод формирующий массив из 8 случайных целых чисел

---
+++ homework_4/task_3/Program.cs
@@ -0,0 +1,37 @@
+﻿/*
+Задача 29: Напишите программу, реализующую метод, который формирует массив случайных целых чисел 
+из 8 элементов и выводит их на экран.
+1, 2, 5, 7, 19,6, 1, 33 -> [1, 2, 5, 7, 19,6, 1, 33 ]
+*/
+
+using static System.Console;
+Clear();
+
+int[] arrey = IntArrey();
+PrintArrey(arrey);
+
+
+int[] IntArrey()
+{
+    int[] res = new int[8];
+
+    for (int i = 0; i < res.Length; i++)
+    {
+        Random ran = new Random();
+        res [i] = ran.Next(0, 99);
+    }
+
+    return res;
+}
+
+void PrintArrey(int[] arr)
+{
+    int count = arr.Length;
+    
+    for (int i = 0; i < count; i++)
+    {
+        Console.Write($"{arr[i]} ");
+    }
+    
+    Console.WriteLine();
+}
