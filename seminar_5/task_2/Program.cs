d28624d15968ba378808d11acb769fc4a898eb90
vitaly <vitaly.zutov@yandex.ru>
2023-01-20 11:24:06 +0700

    Задача 32. Заменить положительные эл. массива на отрицательные и на оборот

---
+++ seminar_5/task_2/Program.cs
@@ -0,0 +1,36 @@
+﻿/* 
+Задача 32. Напишите программу для заменны элементов массива.
+Положительные элементы заменяются на соответствующие отрицательные
+и на оборот.
+Например: [-4, -8, 8, 2] [4, 8, -8, -2]
+*/
+
+using static System.Console;
+Clear();
+
+
+int[] arrey = { -1, -2, 0, 2, 1, 3, 4 };
+
+
+ReverseArrey(arrey);
+PrintArray(arrey);
+
+
+void ReverseArrey(int[] inArrey)
+{
+    for(int i = 0; i < inArrey.Length; i++)
+    {
+        inArrey[i] *= -1;   
+    }
+}
+
+
+void PrintArray(int[] inArray)
+{
+    Write("[");
+    for (int i = 0; i < inArray.Length-1; i++)
+    {
+        Write($"{inArray[i]},");
+    }
+    Write($"{inArray[inArray.Length-1]}]");
+}
