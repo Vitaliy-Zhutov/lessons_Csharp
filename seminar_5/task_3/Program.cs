02910034839a56550353433c8777047ae64fcc2d
vitaly <vitaly.zutov@yandex.ru>
2023-01-20 11:27:30 +0700

    Задача 33. Праграмму, определяет, присутствует ли заданное число в массиве

---
+++ seminar_5/task_3/Program.cs
@@ -0,0 +1,25 @@
+﻿/* 
+Задача 33. Напишите программу, которая определяет,
+присутствует ли заданное числое в массиве.
+4; массив [6, 7, 19, 345, 3] -> нет
+3; массив [6, 7, 19, 345, 3] -> да
+*/
+
+using static System.Console;
+Clear();
+
+Write("Введите число: ");
+int number = int.Parse(ReadLine()!);
+
+int[] array = { -1, -2, 0, 2, 1, 3, 4 };
+
+WriteLine(ArreyLookUp(array, number) ? "да" : "нет");
+
+bool ArreyLookUp(int[] inArray, int num)
+{
+    foreach (int i in inArray)
+    {
+        if (num == i) return true;
+    }
+    return false;
+}
