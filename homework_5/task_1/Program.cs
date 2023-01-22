a701edba54fd0f81a9a96be5a54040f5cb9965fc
vitaly <vitaly.zutov@yandex.ru>
2023-01-22 14:25:51 +0700

    Задача 34. Массив заполнен случайными 3-х зн. числами. Подсчитываем количество четных чисел

---
+++ homework_5/task_1/Program.cs
@@ -0,0 +1,46 @@
+﻿/*
+Задача 34: Напишите программу реализующую методы формирования массива, 
+заполненного случайными положительными трёхзначными числами, 
+и подсчета количества чётных чисел в массиве.
+[345, 897, 568, 234] -> 2
+*/
+
+using static System.Console;
+Clear();
+
+Console.WriteLine("Enter the size of the array");
+int size = int.Parse(ReadLine()!);
+int[] numbers = new int[size];
+
+FillArrayRandomNumbers(numbers);
+Console.WriteLine("Our array: ");
+PrintArray(numbers);
+
+
+int count = 0;
+for (int j = 0; j < numbers.Length; j++)
+    if (numbers[j] % 2 == 0)
+        count++;
+
+
+Console.WriteLine($"All {numbers.Length} numbers, {count} even numbers");
+
+
+void FillArrayRandomNumbers(int[] numbers)
+{
+    for (int i = 0; i < numbers.Length; i++)
+    {
+        numbers[i] = new Random().Next(100, 1000);
+    }
+}
+
+
+void PrintArray(int[] inArray)
+{
+    Write("[");
+    for (int i = 0; i < inArray.Length - 1; i++)
+    {
+        Write($"{inArray[i]}, ");
+    }
+    WriteLine($"{inArray[inArray.Length - 1]}]");
+}
