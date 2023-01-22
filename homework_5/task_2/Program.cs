b959e6a596b39b15527fa0aedf8a5e89d5fd8b0d
vitaly <vitaly.zutov@yandex.ru>
2023-01-22 14:40:14 +0700

    Задача 36. В массиве со случайными числами подсчитываем числа на нечетных позициях

---
+++ homework_5/task_2/Program.cs
@@ -0,0 +1,46 @@
+﻿/*
+Задача 36: Напишите программу реализующую методы формирования массива, 
+заполненного случайными числами и подсчета суммы элементов, 
+стоящих на нечётных позициях.
+[3, 7, 23, 12] -> 19
+[-4, -6, 89, 6] -> 0
+*/
+
+using static System.Console;
+Clear();
+
+Console.WriteLine("Enter the size of the array");
+int size = int.Parse(ReadLine()!);
+int[] numbers = new int[size];
+
+FillArrayRandomNumbers(numbers);
+Console.WriteLine("Our array: ");
+PrintArray(numbers);
+
+
+int sum = 0;
+for (int j = 0; j < numbers.Length; j += 2)
+    sum = sum + numbers[j];
+
+
+Console.WriteLine($"All {numbers.Length} numbers, the sum of the numbers in odd positions = {sum}");
+
+
+void FillArrayRandomNumbers(int[] numbers)
+{
+    for (int i = 0; i < numbers.Length; i++)
+    {
+        numbers[i] = new Random().Next(1, 10);
+    }
+}
+
+
+void PrintArray(int[] inArray)
+{
+    Write("[");
+    for (int i = 0; i < inArray.Length - 1; i++)
+    {
+        Write($"{inArray[i]}, ");
+    }
+    WriteLine($"{inArray[inArray.Length - 1]}]");
+}
