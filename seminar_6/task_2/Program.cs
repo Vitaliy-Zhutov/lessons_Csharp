9043064b019ad40213d81fa7cef9a9384a4f0c9a
vitaly <vitaly.zutov@yandex.ru>
2023-01-22 22:37:10 +0700

    Принимаем на вход 3 числа, проверка может ли сущест. треугольник такой длинны

---
+++ seminar_6/task_2/Program.cs
@@ -0,0 +1,33 @@
+﻿/*
+Напишите программу, которая принимает на вход 3 числа
+и проверяет, может ли существовать треугольник такой длинны.
+*/
+
+using static System.Console;
+Clear();
+
+int a = AskUserForNumber("Введите сторону треугольника a: ");
+int b = AskUserForNumber("Введите сторону треугольника b: ");
+int c = AskUserForNumber("Введите сторону треугольника c: ");
+
+WriteLine(IsTriangle(a, b, c) ? $"Треугольник со сторонами {a}, {b} и {c} существует" : 
+$"Треугольник со сторонами {a}, {b} и {c} не существует");
+
+
+bool IsTriangle(int inA, int inB, int inC)
+{
+    return (inA + inB > inC && inA + inC > inB && inB + inC > inA);
+}
+
+
+int AskUserForNumber(string inPromot)
+{
+    int resultNum = 0;
+    string userInput = "";
+    while (!int.TryParse(userInput, out resultNum))
+    {
+        Console.Write(inPromot);
+        userInput = Console.ReadLine()!;
+    }
+    return resultNum;
+}
