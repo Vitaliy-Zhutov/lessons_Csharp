bf244d84883030b3d322762a3231390d758e86f7
vitaly <vitaly.zutov@yandex.ru>
2023-01-22 22:39:07 +0700

    Задача 42. Преобразование десятичного числа в двоичное

---
+++ seminar_6/task_3/Program.cs
@@ -0,0 +1,44 @@
+﻿/*
+Задача 42. Напишите программу, которая будет проиобразовывать десятичное число,
+в двоичное.
+*/
+
+using static System.Console;
+Clear();
+
+int num = AskUserForNumber("Введите число для перевода в двоичную систему: ");
+
+WriteLine($"Десятичное число - {num}, в двоичном представлении будет - {decimalToBinary (num)}.");
+
+
+string decimalToBinary (int inNum)
+{
+    if (inNum == 0) return "0";
+    string result = string.Empty;
+    // Остаток - remainder -> rem
+    int rem;
+
+    while (inNum > 0)
+    {
+        rem = inNum % 2;
+        inNum /= 2;
+        result = rem.ToString() + result;
+    }
+
+    return result;
+}
+
+
+
+int AskUserForNumber(string inPromot)
+{
+    int resultNum = 0;
+    string userInput = "";
+    while (!int.TryParse(userInput, out resultNum))
+    {
+        Console.Write(inPromot);
+        userInput = Console.ReadLine()!;
+    }
+    return resultNum;
+}
+
