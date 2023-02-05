31da15049be3197f37fe18625a5ae10cafe276bb
vitaly <vitaly.zutov@yandex.ru>
2023-02-05 22:34:55 +0700

    Задача 66. Рекурсивный метод нахождения суммы эл. в промежутке от M до N

---
+++ homework_9/task_2/Program.cs
@@ -0,0 +1,37 @@
+﻿/*
+Задача 66. Напишите программу, которая реализует рекурсивный метод 
+нахождения суммы натуральных элементов в промежутке от M до N.
+M = 1; N = 15 -> 120
+M = 4; N = 8. -> 30
+*/
+
+using static System.Console;
+Clear();
+
+Write("Введите число M: ");
+int m = int.Parse(ReadLine()!);
+
+Write("Введите число N: ");
+int n = int.Parse(ReadLine()!);
+
+SumFromMToN(m, n);
+
+
+void SumFromMToN(int m, int n)
+{
+    Write($"Сумма элементов в промежутке от M до N = {SumMN(m - 1, n)}");
+}
+
+
+int SumMN(int m, int n)
+{
+    int res = m;
+    if (m == n)
+        return 0;
+    else
+    {
+        m++;
+        res = m + SumMN(m, n);
+        return res;
+    }
+}
